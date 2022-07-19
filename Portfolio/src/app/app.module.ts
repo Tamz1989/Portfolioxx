06bcadaa0e8e2d08dbc7ec079f1d2fc03003d850
Tamz1989 <tamaragrimal1989@gmail.com>
2022-07-19 06:16:29 -0300

    3
    3

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgComponent
+    LogoArgComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
