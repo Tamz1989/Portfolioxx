a7322dc3bc6cb89f08cc970c20b303f44cdf1367
Tamz1989 <tamaragrimal1989@gmail.com>
2022-07-19 05:40:04 -0300

    3

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgComponent
   ],
   imports: [
     BrowserModule,
